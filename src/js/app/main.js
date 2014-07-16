6f3ff252c69ebfa4d2e8ac8b9f8f5d20e8bfe0a3
Jesse <jesse.luoto@gmail.com>
2014-07-16 14:14:12 +0300

    Added: Refresh after all read

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,4 +1,11 @@
+var j = {};
 
+$(function() {
+    
+    setTimeout(function() {
+        j.SwipeBook.refresh();
+    }, 100);
+    
     window.fbAsyncInit = function() {
         FB.init({
           appId      : '816021041756653',
@@ -8,196 +15,219 @@
           version    : 'v2.0' // use version 2.0
         });
         
+        
         var tryLogin = setInterval(function() {
             FB.getLoginStatus(function(response) {
                 if (response.status === 'connected') {
                     j.SwipeBook.run();
                     clearInterval(tryLogin);
+                    $("body").removeClass("login");
+                } else {
+                    $("body").removeClass("loading");
+                    $("body").addClass("login");
                 }
+                $("body").removeClass("initializing");
             });
         }, 1000);
     };
     
-    var j = {};
     
-    $(function() {
-        j.SwipeBook = (function() {
+    j.SwipeBook = (function() {
+        
+        var templates = ["status",
+                         "photo",
+                         "link"];
+        
+        function SwipeBook() {
+            j.templates = {};
+            templates.forEach(function(templateName) {
+                j.templates[templateName] = Handlebars.compile($("#template-" + templateName).html());
+            });
+        }
+        
+        SwipeBook.prototype.run = function() {
+            this.refresh();
+            this.fetch();
+        };
+        
+        SwipeBook.prototype.fetch = function(next, since) {
+            since = since || j.entries.most_recent;
             
-            var templates = ["status",
-                             "photo",
-                             "link"];
-            
-            function SwipeBook() {
-                j.templates = {};
-                templates.forEach(function(templateName) {
-                    j.templates[templateName] = Handlebars.compile($("#template-" + templateName).html());
-                });
-            }
-            
-            SwipeBook.prototype.run = function() {
-                this.refresh();
-                this.fetch(false, j.entries.most_recent);
+            var params = {
+                limit: 3,
+                since: Math.floor(since.getTime()/1000)
             };
             
-            SwipeBook.prototype.fetch = function(next, since) {
-                var params = {
-                    limit: 3,
-                    since: Math.floor(since.getTime()/1000)
-                };
-                
-                var self = this;
-                var callback = function(response) {
-                    console.log(response);
-                    response.data.forEach(function(entry) {
-                        j.entries.Add(entry.id, entry);
-                    });
-                    self.refresh();
-                    if(response.data.length > 0) {
-                        self.fetch(response.paging.next, since);
-                    }
-                };
-                
-                if(!next) {
-                    FB.api('/me/home', 'get', params, callback);
+            $("body").addClass("loading");
+            
+            var self = this;
+            var callback = function(response) {
+                response.data.forEach(function(entry) {
+                    j.entries.Add(entry.id, entry);
+                });
+                self.refresh();
+                if(response.data.length > 0) {
+                    self.fetch(response.paging.next, since);
                 } else {
-                    FB.api(next, 'get', {since: params.since}, callback);
+                    $("body").removeClass("loading");
                 }
             };
             
-            SwipeBook.prototype.refresh = function() {
-                j.entries.entries.forEach(function(entry) {
-                    if(!entry) return;
-                    if($("#" + entry.id).length !== 0) return;
-                    
-                    //console.log(entry);
-                    if( !j.templates[entry.type] ) return;// console.log(entry.type);
-                    
-                    var $template = $("<div />").html(j.templates[entry.type](entry)).appendTo($("#feed")).addClass("composite").attr("id", entry.id);
-                    
-                    // Set dragging
-                    var dragStart = 0;
-                    var winWidth = $(window).width();
-                    var lastPoint = 0;
-                    var prevented = false;
-                    $template.get(0).addEventListener("touchstart", function(e) {
-                        prevented = false;
-                        dragStart = e.touches[0].clientX;
-                        $template.removeClass("animateSwipe");
-                    }, false);
-                    $template.get(0).addEventListener("touchmove", function(e) {
-                        var currPoint = e.touches[0].clientX;
-                        var diff = currPoint - dragStart;
-                        var diffPercent = Math.max(-1,Math.min(1, diff/(winWidth/2)));
-                        lastPoint = diffPercent;
-                        
-                        if(Math.abs(diffPercent) > 0.1 || prevented) {
-                            e.preventDefault();
-                            prevented = true;
-                        } else if(!prevented) {
-                            return;
-                        }
-                        
-                        $template.css({
-                            "-webkit-transform": "rotateZ("+(diffPercent * 45)+"deg)",
-                            "opacity": 1-Math.abs(diffPercent)
-                        });
-                    }, false);
-                    $template.get(0).addEventListener("touchend", function(e) {
-                        $template.addClass("animateSwipe");
-                        
-                        if(Math.abs(lastPoint) > 0.55) {
-                            var deg = lastPoint > 0 ? 90 : -90;
-                            $template.slideUp();
-                            $template.css({
-                                "-webkit-transform": "rotateZ("+deg+"deg)",
-                                "opacity": "0%"
-                            });
-                            j.entries.Delete(entry.id);
-                        } else {
-                            $template.removeAttr("style");
-                        }
-                    }, false);
-                    
-                    // Grab additional parameters
-                    /*if(entry.type == "photo") {
-                        FB.api(entry.object_id, 'get', {}, function(data) {
-                            entry.postfetch = data;
-                            $template.html( j.templates[entry.type](entry) );
-                        });
-                    }*/
-                });
-            };
-            
-            return new SwipeBook();
-            
-        })();
+            if(!next) {
+                FB.api('/me/home', 'get', params, callback);
+            } else {
+                FB.api(next, 'get', {since: params.since}, callback);
+            }
+        };
         
-        j.Login = function() {
-            FB.login(null, {
-                scope: 'read_stream', 
-                return_scopes: true
+        SwipeBook.prototype.refresh = function() {
+            var hasEntries = false;
+            
+            j.entries.entries.forEach(function(entry) {
+                if(!entry) return;
+                if( !j.templates[entry.type] ) return;// console.log(entry.type);
+                if( entry.type == "status" && !entry.message ) return;// console.log(entry.type);
+                hasEntries = true;
+                if($("#" + entry.id).length !== 0) return;
+                
+                //console.log(entry);
+                var $template = $("<div />").html(j.templates[entry.type](entry)).appendTo($("#feed")).addClass("composite").attr("id", entry.id);
+                
+                // Set dragging
+                var dragStart = 0;
+                var winWidth = $(window).width();
+                var lastPoint = 0;
+                var prevented = false;
+                $template.get(0).addEventListener("touchstart", function(e) {
+                    prevented = false;
+                    dragStart = e.touches[0].clientX;
+                    $template.removeClass("animateSwipe");
+                }, false);
+                $template.get(0).addEventListener("touchmove", function(e) {
+                    var currPoint = e.touches[0].clientX;
+                    var diff = currPoint - dragStart;
+                    var diffPercent = Math.max(-1,Math.min(1, diff/(winWidth/2)));
+                    lastPoint = diffPercent;
+                    
+                    if(Math.abs(diffPercent) > 0.1 || prevented) {
+                        e.preventDefault();
+                        prevented = true;
+                    } else if(!prevented) {
+                        return;
+                    }
+                    
+                    $template.css({
+                        "-webkit-transform": "rotateZ("+(diffPercent * 45)+"deg)",
+                        "opacity": 1-Math.abs(diffPercent)
+                    });
+                }, false);
+                $template.get(0).addEventListener("touchend", function(e) {
+                    $template.addClass("animateSwipe");
+                    
+                    if(Math.abs(lastPoint) > 0.55) {
+                        var deg = lastPoint > 0 ? 90 : -90;
+                        $template.slideUp();
+                        $template.css({
+                            "-webkit-transform": "rotateZ("+deg+"deg)",
+                            "opacity": "0%"
+                        });
+                        j.entries.Delete(entry.id);
+                        j.SwipeBook.refresh();
+                    } else {
+                        $template.removeAttr("style");
+                    }
+                }, false);
+                
+                // Grab additional parameters
+                /*if(entry.type == "photo") {
+                    FB.api(entry.object_id, 'get', {}, function(data) {
+                        entry.postfetch = data;
+                        $template.html( j.templates[entry.type](entry) );
+                    });
+                }*/
+            });
+            
+            if(!hasEntries) {
+                $("body").addClass("allread");
+            } else {
+                $("body").removeClass("allread");
+            }
+        };
+        
+        return new SwipeBook();
+        
+    })();
+    
+    j.Login = function() {
+        FB.login(null, {
+            scope: 'read_stream', 
+            return_scopes: true
+        });
+    };
+    
+    j.Logout = function() {
+        FB.logout();
+        $("body").removeClass("login");
+    };
+    
+    j.entries = (function() {
+        
+        function Entries() {
+            this.entries = [];
+            for( var i in localStorage ) {
+                if( i.indexOf("entry-") === 0 ) {
+                    var entry = JSON.parse(localStorage[i]);
+                    this.entries.push(entry);
+                }
+            }
+            this.Refresh();
+            var most_recent = localStorage.getItem("most_recent");
+            this.most_recent = most_recent ? new Date(most_recent) : new Date(new Date().getTime() - (1000*60*60*3));
+        }
+        
+        Entries.prototype.Delete = function(id) {
+            localStorage.setItem("entry-" + id, false);
+            for( var i in this.entries ){
+                if(this.entries[i].id == id) {
+                    delete this.entries[i];
+                    return;
+                }
+            }
+            this.Refresh();
+        };
+        
+        Entries.prototype.Refresh = function() {
+            this.entries.sort(function(x, y){
+                if(!x && !y) return 0;
+                if(!x) return -1;
+                if(!y) return -1;
+                
+                if (new Date(x.updated_time) < new Date(y.updated_time)) {
+                    return -1;
+                }
+                if (new Date(x.updated_time) > new Date(y.updated_time)) {
+                    return 1;
+                }
+                return 0;
             });
         };
         
-        j.entries = (function() {
+        Entries.prototype.Add = function(id, entry) {
+            if( localStorage.getItem("entry-" + id) !== null ) return;
+            this.entries.push(entry);
+            localStorage.setItem("entry-" + id, JSON.stringify(entry));
             
-            function Entries() {
-                this.entries = [];
-                for( var i in localStorage ) {
-                    if( i.indexOf("entry-") === 0 ) {
-                        var entry = JSON.parse(localStorage[i]);
-                        this.entries.push(entry);
-                    }
+            if(entry.updated_time) {
+                var entry_time = new Date(entry.updated_time);
+                if( entry_time > this.most_recent ) {
+                    this.most_recent = entry_time;
+                    localStorage.setItem("most_recent", entry_time);
                 }
-                this.Refresh();
-                var most_recent = localStorage.getItem("most_recent");
-                this.most_recent = most_recent ? new Date(most_recent) : new Date(new Date().getTime() - (1000*60*60*3));
             }
-            
-            Entries.prototype.Delete = function(id) {
-                localStorage.setItem("entry-" + id, false);
-                for( var i in this.entries ){
-                    if(this.entries[i].id == id) {
-                        delete this.entries[i];
-                        return;
-                    }
-                }
-                this.Refresh();
-            };
-            
-            Entries.prototype.Refresh = function() {
-                this.entries.sort(function(x, y){
-                    if(!x && !y) return 0;
-                    if(!x) return -1;
-                    if(!y) return -1;
-                    
-                    if (new Date(x.updated_time) < new Date(y.updated_time)) {
-                        return -1;
-                    }
-                    if (new Date(x.updated_time) > new Date(y.updated_time)) {
-                        return 1;
-                    }
-                    return 0;
-                });
-            }
-            
-            Entries.prototype.Add = function(id, entry) {
-                if( localStorage.getItem("entry-" + id) !== null ) return;
-                this.entries.push(entry);
-                localStorage.setItem("entry-" + id, JSON.stringify(entry));
-                
-                if(entry.updated_time) {
-                    var entry_time = new Date(entry.updated_time);
-                    if( entry_time > this.most_recent ) {
-                        this.most_recent = entry_time;
-                        localStorage.setItem("most_recent", entry_time);
-                    }
-                }
-                this.Refresh();
-            };
-            
-            
-            return new Entries();
-        })();
-    });
-    
+            this.Refresh();
+        };
+        
+        
+        return new Entries();
+    })();
+});
--- swipebook/js/app/main.js
+++ swipebook/js/app/main.js
@@ -1,21 +1,22 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: "816021041756653",
-        cookie: !0,
-        xfbml: !0,
-        version: "v2.0"
-    });
-    var a = setInterval(function() {
-        FB.getLoginStatus(function(b) {
-            "connected" === b.status && (j.SwipeBook.run(), clearInterval(a));
-        });
-    }, 1e3);
-};
-
 var j = {};
 
 $(function() {
-    j.SwipeBook = function() {
+    setTimeout(function() {
+        j.SwipeBook.refresh();
+    }, 100), window.fbAsyncInit = function() {
+        FB.init({
+            appId: "816021041756653",
+            cookie: !0,
+            xfbml: !0,
+            version: "v2.0"
+        });
+        var a = setInterval(function() {
+            FB.getLoginStatus(function(b) {
+                "connected" === b.status ? (j.SwipeBook.run(), clearInterval(a), $("body").removeClass("login")) : ($("body").removeClass("loading"), 
+                $("body").addClass("login")), $("body").removeClass("initializing");
+            });
+        }, 1e3);
+    }, j.SwipeBook = function() {
         function a() {
             j.templates = {}, b.forEach(function(a) {
                 j.templates[a] = Handlebars.compile($("#template-" + a).html());
@@ -23,49 +24,55 @@ $(function() {
         }
         var b = [ "status", "photo", "link" ];
         return a.prototype.run = function() {
-            this.refresh(), this.fetch(!1, j.entries.most_recent);
+            this.refresh(), this.fetch();
         }, a.prototype.fetch = function(a, b) {
+            b = b || j.entries.most_recent;
             var c = {
                 limit: 3,
                 since: Math.floor(b.getTime() / 1e3)
-            }, d = this, e = function(a) {
-                console.log(a), a.data.forEach(function(a) {
+            };
+            $("body").addClass("loading");
+            var d = this, e = function(a) {
+                a.data.forEach(function(a) {
                     j.entries.Add(a.id, a);
-                }), d.refresh(), a.data.length > 0 && d.fetch(a.paging.next, b);
+                }), d.refresh(), a.data.length > 0 ? d.fetch(a.paging.next, b) : $("body").removeClass("loading");
             };
             a ? FB.api(a, "get", {
                 since: c.since
             }, e) : FB.api("/me/home", "get", c, e);
         }, a.prototype.refresh = function() {
-            j.entries.entries.forEach(function(a) {
-                if (a && 0 === $("#" + a.id).length && j.templates[a.type]) {
-                    var b = $("<div />").html(j.templates[a.type](a)).appendTo($("#feed")).addClass("composite").attr("id", a.id), c = 0, d = $(window).width(), e = 0, f = !1;
-                    b.get(0).addEventListener("touchstart", function(a) {
-                        f = !1, c = a.touches[0].clientX, b.removeClass("animateSwipe");
-                    }, !1), b.get(0).addEventListener("touchmove", function(a) {
-                        var g = a.touches[0].clientX, h = g - c, i = Math.max(-1, Math.min(1, h / (d / 2)));
-                        if (e = i, Math.abs(i) > .1 || f) a.preventDefault(), f = !0; else if (!f) return;
-                        b.css({
+            var a = !1;
+            j.entries.entries.forEach(function(b) {
+                if (b && j.templates[b.type] && ("status" != b.type || b.message) && (a = !0, 0 === $("#" + b.id).length)) {
+                    var c = $("<div />").html(j.templates[b.type](b)).appendTo($("#feed")).addClass("composite").attr("id", b.id), d = 0, e = $(window).width(), f = 0, g = !1;
+                    c.get(0).addEventListener("touchstart", function(a) {
+                        g = !1, d = a.touches[0].clientX, c.removeClass("animateSwipe");
+                    }, !1), c.get(0).addEventListener("touchmove", function(a) {
+                        var b = a.touches[0].clientX, h = b - d, i = Math.max(-1, Math.min(1, h / (e / 2)));
+                        if (f = i, Math.abs(i) > .1 || g) a.preventDefault(), g = !0; else if (!g) return;
+                        c.css({
                             "-webkit-transform": "rotateZ(" + 45 * i + "deg)",
                             opacity: 1 - Math.abs(i)
                         });
-                    }, !1), b.get(0).addEventListener("touchend", function() {
-                        if (b.addClass("animateSwipe"), Math.abs(e) > .55) {
-                            var c = e > 0 ? 90 : -90;
-                            b.slideUp(), b.css({
-                                "-webkit-transform": "rotateZ(" + c + "deg)",
+                    }, !1), c.get(0).addEventListener("touchend", function() {
+                        if (c.addClass("animateSwipe"), Math.abs(f) > .55) {
+                            var a = f > 0 ? 90 : -90;
+                            c.slideUp(), c.css({
+                                "-webkit-transform": "rotateZ(" + a + "deg)",
                                 opacity: "0%"
-                            }), j.entries.Delete(a.id);
-                        } else b.removeAttr("style");
+                            }), j.entries.Delete(b.id), j.SwipeBook.refresh();
+                        } else c.removeAttr("style");
                     }, !1);
                 }
-            });
+            }), a ? $("body").removeClass("allread") : $("body").addClass("allread");
         }, new a();
     }(), j.Login = function() {
         FB.login(null, {
             scope: "read_stream",
             return_scopes: !0
         });
+    }, j.Logout = function() {
+        FB.logout(), $("body").removeClass("login");
     }, j.entries = function() {
         function a() {
             this.entries = [];
@@ -73,20 +80,26 @@ $(function() {
                 var b = JSON.parse(localStorage[a]);
                 this.entries.push(b);
             }
-            this.entries.sort(function(a, b) {
-                return a || b ? a && b ? new Date(a) < new Date(b) ? -1 : new Date(a) > new Date(b) ? 1 : 0 : -1 : 0;
-            });
+            this.Refresh();
             var c = localStorage.getItem("most_recent");
             this.most_recent = new Date(c ? c : new Date().getTime() - 108e5);
         }
         return a.prototype.Delete = function(a) {
             localStorage.setItem("entry-" + a, !1);
             for (var b in this.entries) if (this.entries[b].id == a) return void delete this.entries[b];
+            this.Refresh();
+        }, a.prototype.Refresh = function() {
+            this.entries.sort(function(a, b) {
+                return a || b ? a && b ? new Date(a.updated_time) < new Date(b.updated_time) ? -1 : new Date(a.updated_time) > new Date(b.updated_time) ? 1 : 0 : -1 : 0;
+            });
         }, a.prototype.Add = function(a, b) {
-            if (null === localStorage.getItem("entry-" + a) && (this.entries.push(b), localStorage.setItem("entry-" + a, JSON.stringify(b)), 
-            b.updated_time)) {
-                var c = new Date(b.updated_time);
-                c > this.most_recent && (this.most_recent = c, localStorage.setItem("most_recent", c));
+            if (null === localStorage.getItem("entry-" + a)) {
+                if (this.entries.push(b), localStorage.setItem("entry-" + a, JSON.stringify(b)), 
+                b.updated_time) {
+                    var c = new Date(b.updated_time);
+                    c > this.most_recent && (this.most_recent = c, localStorage.setItem("most_recent", c));
+                }
+                this.Refresh();
             }
         }, new a();
     }();
